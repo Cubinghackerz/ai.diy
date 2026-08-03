b5da32ceebc67132cf6d6587d2f8d9f4cc9d5d16
Cubinghackerz <cubinghackerz@gmail.com>
2026-08-03 18:23:50 +0800

    fix: import ScrollTrigger default export for Vercel SSR

--- app/routes/landing.tsx
+++ app/routes/landing.tsx
@@ -2,7 +2,7 @@ import { useEffect, useRef, useState } from "react";
 import { Link } from "react-router";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
+import ScrollTrigger from "gsap/ScrollTrigger";
 import {
     ArrowRight,
     ArrowUpRight,
